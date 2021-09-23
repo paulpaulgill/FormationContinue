c0de2c31f4d8453f7cb1db78bd6a46b7b6776919
Gimalac, Antoine <gimalac.antoine@courrier.uqam.ca>
2021-09-23 13:38:41 +0000

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,15 @@
+
+
 public class main {
     public static void main(String args[]) throws Exception {
 
         import_export deux = new import_export("/Users/antoinegimalac/IdeaProjects/projet/src/person.json", "/Users/antoinegimalac/IdeaProjects/projet/src/resultat.json");
         deux.chargement();
-        deux.to_string();
+
         deux.recherche_erreur();
+       // deux.to_string();
         deux.exportation_erreur();
     }
 }
+
+
