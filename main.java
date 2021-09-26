e01e414f94192c2469ecb8424c7180a77649536b
Munier, Elsa <munier.elsa@courrier.uqam.ca>
2021-09-26 15:57:06 +0000

    Update main.java

--- main.java
+++ main.java
@@ -1,13 +1,16 @@
 
-
 public class main {
     public static void main(String args[]) throws Exception {
 
         import_export deux = new import_export("/Users/antoinegimalac/IdeaProjects/projet/src/person.json", "/Users/antoinegimalac/IdeaProjects/projet/src/resultat.json");
         deux.chargement();
+        deux.to_string();
+        
+        deux.Categories();
+        deux.VerificationCycle();
+        deux.CategoriesReconnues();
+        deux.Verification40Heures();
 
-        deux.recherche_erreur();
-       // deux.to_string();
         deux.exportation_erreur();
     }
 }
