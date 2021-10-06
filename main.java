eacc3faf2c4daf9df307ee7278bcc888129542c5
Gimalac, Antoine <gimalac.antoine@courrier.uqam.ca>
2021-10-06 14:52:31 +0000

    nettoyage et appelle méthode ajouté

--- main.java
+++ main.java
@@ -4,11 +4,8 @@ public class main {
         import_export deux = new import_export("declaration.json", "resultat.json");
         deux.chargement();
         deux.validerDate();
-       // System.out.println(deux.verificationDate(0));
-       // System.out.println(deux.verificationDate(1));
         deux.Categories();
-        //deux.verificationCycle();
-        //deux.categoriesReconnues();
+        deux.verification17Heurescategories();
         deux.verification40Heures();
         deux.exportation_erreur();
     }
