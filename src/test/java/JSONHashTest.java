566e1d6fb037924d662702df162efc94219d6b1e
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2021-11-03 19:28:27 -0400

    debuter methodes de test

--- src/test/java/JSONHashTest.java
+++ src/test/java/JSONHashTest.java
@@ -1,7 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JSONHashTest {
+    Profession p1;
+    Profession p2;
+    Profession p3;
+
+    @BeforeEach
+    void setUp(){
+        p1 = new Architectes();
+        p2 = new Geologues();
+        p3 = new Psychologues();
+    }
+
+    @AfterEach
+    void tearDown(){
+        p1 = null;
+        p2 = null;
+        p3 = null;
+    }
+
 
 }
