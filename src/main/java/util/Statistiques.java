9cdd6dcd40f4513411cbfd7cefa763127e7af843
Gill, Paul <gill.paul@courrier.uqam.ca>
2021-12-18 15:45:22 -0500

    constructeur avec ses JsonProperty et charcheur du fichier stat

--- src/main/java/util/Statistiques.java
+++ src/main/java/util/Statistiques.java
@@ -1,5 +1,7 @@
 package util;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.util.DefaultIndenter;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,6 +30,43 @@ public class Statistiques {
     private ObjectMapper objectMapper = new ObjectMapper();
     private DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
 
+    @JsonCreator
+    public Statistiques(@JsonProperty("declarationTraitees") int declarationTraitees,
+                        @JsonProperty("declarationCompletes")int declarationCompletes,
+                        @JsonProperty("declarationIncompletesInvalides")int declarationIncompletesInvalides,
+                        @JsonProperty("declarationHomme")int declarationHomme,
+                        @JsonProperty("declarationFemme")int declarationFemme,
+                        @JsonProperty("declarationSexeInconnu")int declarationSexeInconnu,
+                        @JsonProperty("totalActivites")int totalActivites,
+                        @JsonProperty("activitesParCat")ArrayList<Integer> activitesParCat,
+                        @JsonProperty("declarationCompletesParOrdre")ArrayList<Integer> declarationCompletesParOrdre,
+                        @JsonProperty("declarationIncompletesParOrdre")ArrayList<Integer> declarationIncompletesParOrdre,
+                        @JsonProperty("declarationPermisInvalide")int declarationPermisInvalide) {
+        this.declarationTraitees = declarationTraitees;
+        this.declarationCompletes = declarationCompletes;
+        this.declarationIncompletesInvalides = declarationIncompletesInvalides;
+        this.declarationHomme = declarationHomme;
+        this.declarationFemme = declarationFemme;
+        this.declarationSexeInconnu = declarationSexeInconnu;
+        this.totalActivites = totalActivites;
+        this.activitesParCat = activitesParCat;
+        this.declarationCompletesParOrdre = declarationCompletesParOrdre;
+        this.declarationIncompletesParOrdre = declarationIncompletesParOrdre;
+        this.declarationPermisInvalide = declarationPermisInvalide;
+    }
+
     public Statistiques() {
+    }
+
+    public Statistiques chargerStat() throws FormationContinueException {
+        Statistiques stat;
+        try {
+            stat = objectMapper.readValue(new File("src/main/resources/stat.json"), Statistiques.class);
+        }catch (FileNotFoundException e){
+            stat = new Statistiques();
+        }catch (IOException e){
+            throw new FormationContinueException("erreur inatendu lors de l'importation des statistique");
+        }
+        return stat;
     }
 }
