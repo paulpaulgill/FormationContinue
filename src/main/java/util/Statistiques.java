661eaa35e50a4a9a71113d52050159370c326939
Gill, Paul <gill.paul@courrier.uqam.ca>
2021-12-18 16:17:19 -0500

    ajouter geter seter pour Jackson

--- src/main/java/util/Statistiques.java
+++ src/main/java/util/Statistiques.java
@@ -15,17 +15,17 @@ import java.util.ArrayList;
 
 public class Statistiques {
 
-    int declarationTraitees;
-    int declarationCompletes;
-    int declarationIncompletesInvalides;
-    int declarationHomme;
-    int declarationFemme;
-    int declarationSexeInconnu;
-    int totalActivites;
+    int declarationTraitees = 0;
+    int declarationCompletes = 0;
+    int declarationIncompletesInvalides = 0;
+    int declarationHomme = 0;
+    int declarationFemme = 0;
+    int declarationSexeInconnu = 0;
+    int totalActivites = 0;
     ArrayList<Integer> activitesParCat;
     ArrayList<Integer> declarationCompletesParOrdre;
     ArrayList<Integer> declarationIncompletesParOrdre;
-    int declarationPermisInvalide;
+    int declarationPermisInvalide = 0;
 
     private ObjectMapper objectMapper = new ObjectMapper();
     private DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
@@ -58,15 +58,115 @@ public class Statistiques {
     public Statistiques() {
     }
 
+    public int getDeclarationTraitees() {
+        return declarationTraitees;
+    }
+
+    public void setDeclarationTraitees(int declarationTraitees) {
+        this.declarationTraitees = declarationTraitees;
+    }
+
+    public int getDeclarationCompletes() {
+        return declarationCompletes;
+    }
+
+    public void setDeclarationCompletes(int declarationCompletes) {
+        this.declarationCompletes = declarationCompletes;
+    }
+
+    public int getDeclarationIncompletesInvalides() {
+        return declarationIncompletesInvalides;
+    }
+
+    public void setDeclarationIncompletesInvalides(int declarationIncompletesInvalides) {
+        this.declarationIncompletesInvalides = declarationIncompletesInvalides;
+    }
+
+    public int getDeclarationHomme() {
+        return declarationHomme;
+    }
+
+    public void setDeclarationHomme(int declarationHomme) {
+        this.declarationHomme = declarationHomme;
+    }
+
+    public int getDeclarationFemme() {
+        return declarationFemme;
+    }
+
+    public void setDeclarationFemme(int declarationFemme) {
+        this.declarationFemme = declarationFemme;
+    }
+
+    public int getDeclarationSexeInconnu() {
+        return declarationSexeInconnu;
+    }
+
+    public void setDeclarationSexeInconnu(int declarationSexeInconnu) {
+        this.declarationSexeInconnu = declarationSexeInconnu;
+    }
+
+    public int getTotalActivites() {
+        return totalActivites;
+    }
+
+    public void setTotalActivites(int totalActivites) {
+        this.totalActivites = totalActivites;
+    }
+
+    public ArrayList<Integer> getActivitesParCat() {
+        return activitesParCat;
+    }
+
+    public void setActivitesParCat(ArrayList<Integer> activitesParCat) {
+        this.activitesParCat = activitesParCat;
+    }
+
+    public ArrayList<Integer> getDeclarationCompletesParOrdre() {
+        return declarationCompletesParOrdre;
+    }
+
+    public void setDeclarationCompletesParOrdre(ArrayList<Integer> declarationCompletesParOrdre) {
+        this.declarationCompletesParOrdre = declarationCompletesParOrdre;
+    }
+
+    public ArrayList<Integer> getDeclarationIncompletesParOrdre() {
+        return declarationIncompletesParOrdre;
+    }
+
+    public void setDeclarationIncompletesParOrdre(ArrayList<Integer> declarationIncompletesParOrdre) {
+        this.declarationIncompletesParOrdre = declarationIncompletesParOrdre;
+    }
+
+    public int getDeclarationPermisInvalide() {
+        return declarationPermisInvalide;
+    }
+
+    public void setDeclarationPermisInvalide(int declarationPermisInvalide) {
+        this.declarationPermisInvalide = declarationPermisInvalide;
+    }
+
     public Statistiques chargerStat() throws FormationContinueException {
-        Statistiques stat;
+        Statistiques stat = new Statistiques();
         try {
             stat = objectMapper.readValue(new File("src/main/resources/stat.json"), Statistiques.class);
         }catch (FileNotFoundException e){
-            stat = new Statistiques();
+            stat = creerFichierStat();
         }catch (IOException e){
             throw new FormationContinueException("erreur inatendu lors de l'importation des statistique");
         }
         return stat;
     }
+
+    public Statistiques creerFichierStat() throws FormationContinueException {
+        Statistiques stat = new Statistiques(0,0,0,0,0,0,0,null,null,null,0);
+        pp.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
+        try {
+            objectMapper.writer(pp).writeValue(new File("src/main/resources/stat.json"),stat);
+        } catch (IOException e) {
+            throw new FormationContinueException("erreur inatendu lors de l'importation des statistique");
+        }
+        return stat;
+    }
+
 }
