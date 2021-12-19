33b4c9a51f1c98975d8c37a451f374f0cbb12713
Gill, Paul <gill.paul@courrier.uqam.ca>
2021-12-19 18:09:32 -0500

    revert to new chargement methode

--- src/main/java/util/GestionJSON.java
+++ src/main/java/util/GestionJSON.java
@@ -36,18 +36,13 @@ public class GestionJSON {
     public Declaration chargement() throws FormationContinueException {
         try{
             declaInit = objectMapper.readValue(new File(fichiers_entree), Declaration.class);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, true);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-            objectMapper.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT,false);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
             String ordre = declaInit.getOrdre();
-            if (ordre.equals("architectes")){ declaInit = objectMapper.readValue(new File(fichiers_entree), Architectes.class);}
-            else if (ordre.equals("géologues")){ declaInit = objectMapper.readValue(new File(fichiers_entree), Geologues.class);}
-            else if (ordre.equals("psychologues")){  declaInit = objectMapper.readValue(new File(fichiers_entree), Psychologues.class);}
-            else if (ordre.equals("podiatres")){ declaInit = objectMapper.readValue(new File(fichiers_entree), Podiatres.class);}
+            if (ordre.equals("architectes")){ declaInit = new Architectes();}
+            else if (ordre.equals("géologues")){ declaInit = new Geologues();}
+            else if (ordre.equals("psychologues")){  declaInit = new Psychologues();}
+            else if (ordre.equals("podiatres")){ declaInit = new Podiatres();}
             else{ throw new FormationContinueException("La structure du fichier d'entrée n'est pas respecté");}
-            return declaInit;
+            return chargerType(declaInit.getClass());
         }catch(FileNotFoundException erreur) {
             throw new FormationContinueException("Le fichier donné est introuvable.");
         }catch (JsonMappingException erreur){
@@ -65,6 +60,7 @@ public class GestionJSON {
         objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
         return objectMapper.readValue(new File(fichiers_entree), eClass);
     }
+
 
     /**
      * Écris dans le fichier résultat si le cycle est complet et les erreurs
